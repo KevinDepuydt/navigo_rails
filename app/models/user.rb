222dabd214de3a4e934082ff51524156a8634cfc
KevinDepuydt <kevin.depuydt@supinternet.fr>
2016-12-25 11:21:08 +0100

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
-require 'securerandom'
-
 class User < ApplicationRecord
-  before_create :set_auth_token
-
   has_one :card
 
   devise :database_authenticatable, :registerable,
@@ -10,14 +6,4 @@ class User < ApplicationRecord
 
   has_attached_file :avatar, :styles => { :medium => "300x300", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-
-  private
-  def set_auth_token
-    return if auth_token.present?
-    self.auth_token = generate_auth_token
-  end
-
-  def generate_auth_token
-    SecureRandom.uuid.gsub(/\-/,'')
-  end
 end
